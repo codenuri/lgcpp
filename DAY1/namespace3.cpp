65ffdb70c03935e9a008d3c7a97076a0db2952b8
ecourse <smkang@codenuri.co.kr>
2024-02-16 10:28:32 +0900

    Add files via upload

---
+++ DAY1/namespace3.cpp
@@ -0,0 +1,10 @@
+﻿// std::namespace - 9page
+#include <algorithm>
+
+// 모든 C++ 표준은 "std" 라는 namespace 안에 있습니다
+
+int main()
+{
+//	int n1 = min(3, 2); // C++ 표준 함수. algorithm 헤더 필요
+	int n2 = std::min(3, 2);
+}
---
+++ DAY1/namespace4.cpp
@@ -0,0 +1,20 @@
+﻿// 10 page
+//#include <stdio.h> // C언어용 헤더
+#include <cstdio>    // C++ 에서 다시 만든 헤더
+					 // printf 등이 전역과 "std" 안에 모두 있다
+
+// C++ 은 C 언어용 헤더를 모두 새로운 버전으로도 제공합니다.
+// 규칙 : <xxx.h> ==> <cxxx>
+
+// <stdlib.h>  ==>  <cstdlib>
+// <string.h>  ==>  <cstring>
+// <math.h>    ==>  <cmath>
+
+
+int main()
+{
+	// printf 는 C 표준함수이지만
+	//           C++ 표준이기도 합니다.
+	printf("hello\n"); // ok
+	std::printf("hello\n"); // ??
+}
