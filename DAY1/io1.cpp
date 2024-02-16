8b2c5d8802b7bc4eabc1820f48f2cfec8f188da8
ecourse <smkang@codenuri.co.kr>
2024-02-16 11:06:12 +0900

    Add files via upload

---
+++ DAY1/io1.cpp
@@ -0,0 +1,29 @@
+﻿//#include <cstdio>		// printf를 위해서
+#include <iostream> 	// std::cout 을 위해서, 이헤더만 있으면
+						//					printf 도 사용가능
+
+// 13 page
+int main()
+{
+	int n = 10;
+
+	// C style 입출력
+	printf("n = %d\n", n); // C style
+//	scanf("%d", &n);		// 최신 C/C++ 컴파일러는 에러
+							// 보안문제!! 아래 처럼 하세요
+							
+	scanf_s("%d", &n);	   // 입력
+
+	// C++ style
+	// 1. %d 등의 포맷 지시어를 사용할 필요 없습니다
+	// 2. << 를 연속적으로 사용가능합니다.
+	// 3. 개행은 "/n" 또는 std::endl; 사용
+	// printf    : 함수
+	// std::cout : 객체라는 존재, 지금은 사용법만 외우세요
+	//				5일차에 직접 만들게 됩니다.
+	std::cout << "n = " << n << std::endl;
+
+	// 입력
+	std::cin >> n; 	// &n 이 아닌 n 입니다.
+					// >> 방향 주의 하세요
+}
---
+++ DAY1/io2.cpp
@@ -0,0 +1,28 @@
+﻿// 18page
+#include <iostream>
+#include <iomanip>
+
+// 입출력 조정자(io manipulator)
+// => 출력 모양을 조정할때 사용하는 것(대부분 함수들)
+// => <iostream> 또는 <iomanip> 헤더 필요
+
+int main()
+{
+	int n = 10;
+
+	std::cout << n << std::endl; // 10.  즉, 10진수 출력
+
+	std::cout << std::hex << n << std::endl; // a, 16 진수 출력
+
+	std::cout << std::dec << n << std::endl; // 
+
+	std::cout << std::setw(10) << std::setfill('#') << n << std::endl; // 
+
+	double d = 0.123456789;
+
+	std::cout << d << std::endl; // 몇 자리 까지 나오는지 확인하세요
+								 // 9자리 까지 모두 나오게 해보세요
+
+	std::cout << std::setprecision(10) << d << std::endl;							 
+}
+// cppreference.com 에서 std::setw 검색
