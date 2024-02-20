8f9d77e99f13dd1108668d7cb11ea7cae35fddc3
ecourse <smkang@codenuri.co.kr>
2024-02-20 16:11:13 +0900

    Add files via upload

--- DAY3/date_impl.cpp
+++ DAY3/date_impl.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31,30,31};
+// 월별 날짜를 관리하는 배열
+// 전역으로 놓고 사용하면 편리하긴 하지만, 안전하지 않습니다. 어디서나 접근 가능
+// int days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31, 30, 31};
 
 
 class Date
@@ -10,6 +12,10 @@ private:
 	int month;
 	int day;
 
+//	int days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31, 30, 31};
+
+	static int days[12]; // static 멤버의 초기화는 클래스 외부 선언에서 해야 합니다
+
 public:	
 	Date(int y, int m, int d) : year{y}, month{m}, day{d} 
 	{
@@ -18,11 +24,22 @@ public:
 	{
 		std::cout << year << "-" << month << "-" << day << std::endl;
 	}
+
+	Date tomorrow()
+	{
+		Date d(year, month, day+1);
+		return d;
+	}
 };
+int Date::days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31, 30, 31};
+
+
 
 int main()
 {
 	Date today(2024, 2, 20);
+	today.print();
 
 	Date d = today.tomorrow();
+	d.print(); // 2024-2-21
 }
