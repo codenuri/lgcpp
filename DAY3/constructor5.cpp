d2bf8c8f809a0fe4a89f78f9cc9680126383e690
ecourse <smkang@codenuri.co.kr>
2024-02-20 09:29:53 +0900

    Add files via upload

--- DAY3/constructor5.cpp
+++ DAY3/constructor5.cpp
@@ -22,10 +22,15 @@ public:
 
 		std::cout << "Rect()" << std::endl;
 	}
+	// 멤버 데이타의 생성자는 
+	// 1. 컴파일러가 추가하는 코드는 항상 디폴트 생성자만 호출합니다.
+	// 2. 디폴트가 아닌 다른 생성자를 호출하려면
+	//    개발자가 직접 호출해야 합니다.
+	//    아래 코드 처럼 해야 합니다.
 
-	Rect(int left, int top, int right, int bottom)
+	Rect(int left, int top, int right, int bottom) 
+		: leftTop(left, top), rightBottom(right, bottom)
 	{
-
 		std::cout <<"Rect(int, int, int, int)" << std::endl;
 	}
 };
@@ -36,7 +41,12 @@ int main()
 	Rect rc2(1, 1, 10, 10); 
 }
 
-
+// 객체를 생성하면
+// 1. 객체의 생성자가 호출됩니다.
+// 2. 객체의 생성자에는 "멤버 데이타의 생성자" 를 호출하는 기계어 코드가 포함됩니다.
+// 3. 멤버 데이타의 생성자는 기본적으로는 "디폴트 생성자가 호출" 됩니다.
+// 4. 멤버의 다른 생성자를 호출하려면 사용자가 "초기화 리스트" 문법으로 지정해야합니다
+//                Rect() : 멤버(인자), 멤버(인자)
 
 
 
