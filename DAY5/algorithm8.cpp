3acdb8f914ba7a83d100f28d026327022eaa2491
ecourse <smkang@codenuri.co.kr>
2024-02-22 16:35:43 +0900

    Add files via upload

--- DAY5/algorithm8.cpp
+++ DAY5/algorithm8.cpp
@@ -7,12 +7,29 @@
 
 std::vector<People*> v = { new People("kim", 20), 
 						   new People("lee", 30),
-						   new People("park", 40)};
+						   new People("park", 40),
+						   new People("lee", 35) };
 
 int main()
 {
-	// v에서 이름이 lee 인 사람을 찾아서 나이를 출력해 주세요
-	
+	// v에서 이름이 lee 인 사람을 찾아서 나이를 출력해 주세요 - std::find
+	// std::find : 요소의 특정한 값 찾기..
+	//			   현재 요소는 객체의 주소..!!!
+	//             특정 주소를 찾을 이유는 없다.!!!
+	//             따라서 find_if 로 해야 합니다.
+
+	auto ret = std::find_if( v.begin(), v.end(), 
+
+							[](People* p){ return p->name == "lee"; } );
+
+	if ( ret != v.end() )
+	{
+		std::cout << ret->name << "(" << ret->age << ")\n"; 
+	}
+
+
+	// 2번째 사람도 찾아보세요
+
 }
 
 
