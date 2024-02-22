976c5ec367ed161fa84208aec3605471c8758a27
ecourse <smkang@codenuri.co.kr>
2024-02-22 13:58:22 +0900

    Add files via upload

---
+++ DAY5/iterator0.cpp
@@ -0,0 +1,16 @@
+// iterator0.cpp
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main()
+{
+	// #1. 배열 vs vector
+	int x[5] = {1, 2, 3, 4, 5};
+	
+	std::vector<int> v = {1, 2, 3, 4, 5};
+
+
+
+
+}
