3ee50ce03c74297bcf96051925a46c1557854bda
ecourse <smkang@codenuri.co.kr>
2024-02-23 13:12:18 +0900

    Add files via upload

--- DAY6/casting4.cpp
+++ DAY6/casting4.cpp
@@ -6,8 +6,11 @@
 	double* p1 = (double*)&c; // ok...
 
 	// 위 코드를 C++ 캐스팅으로 해보세요
-	double* p2 = ?;
+	// 서로 다른 타입주소는 "static_cast"는 안됩니다.
+//	double* p2 = reinterpret_cast<double*>(&c); // error. c 는 상수
+//	double* p2 = const_cast<double*>(&c);  // error. 동일 타입의 상수성제거
 
+	double* p2 = reinterpret_cast<double*>(  const_cast<int*>(&c)); // ok
 }
 
 
