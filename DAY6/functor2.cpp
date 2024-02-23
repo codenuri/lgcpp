67528faad0da69277ecdcd5109c60ebbb5385376
ecourse <smkang@codenuri.co.kr>
2024-02-23 09:10:47 +0900

    Add files via upload

---
+++ DAY6/functor2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = {1,9,2,4,3,7};
+
+	// v 에서 1번째 나오는 3의 배수를 찾아보세요 - 람다표현식 말고, 일반함수로
+	auto ret = ?;
+}
