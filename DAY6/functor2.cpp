d581249c480b90e0e0fe73dedb863ca7f1c65769
ecourse <smkang@codenuri.co.kr>
2024-02-23 09:13:19 +0900

    Add files via upload

--- DAY6/functor2.cpp
+++ DAY6/functor2.cpp
@@ -2,10 +2,18 @@
 #include <vector>
 #include <algorithm>
 
+bool foo(int n)
+{
+	return n % 3 == 0;
+}
+
 int main()
 {
 	std::vector<int> v = {1,9,2,4,3,7};
 
 	// v 에서 1번째 나오는 3의 배수를 찾아보세요 - 람다표현식 말고, 일반함수로
-	auto ret = ?;
+	auto ret = std::find_if( v.begin(), v.end(), foo);
+
+	// 일반함수로 3의 배수가 아닌 k의 배수를 찾을수 있을까요 ?
+	int k = 3;
 }
