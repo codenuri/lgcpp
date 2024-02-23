b4e53eacce16813bac9c31db3f18267893de2014
ecourse <smkang@codenuri.co.kr>
2024-02-23 11:01:13 +0900

    Add files via upload

--- DAY6/variable_function.cpp
+++ DAY6/variable_function.cpp
@@ -7,14 +7,16 @@
 
 int sum(int cnt,  ...)
 {
-	va_list ap;
-	va_start(ap, cnt);
+	va_list ap;			// ap는 내부적으로는 pointer 입니다.
+	va_start(ap, cnt);	// ap 를 cnt 다음 인자가 있는 메모리를 가리키게 해달라
 	int s = 0;
 	for( int i = 0; i < cnt; i++)
 	{
 		s += va_arg(ap, int);
+				// 1. ap 위치의 메모리에서 int 한개를 꺼내고, 
+				// 2. ap 는 sizeof(int) 만큼 이동
 	}
-	va_end(ap);
+	va_end(ap); // ap 사용후에는 반드시 va_end(ap) 를 하라고 규칙이 정해져 있음.
 	return s;
 }
 int main()
@@ -23,4 +25,11 @@ int main()
 	// => 1개 이상.
 	int s = sum(5, 1,2,3,4,5);
 	std::cout << s << std::endl;
+
+	int n = 10;
+	double d = 2.1;
+
+	// printf 로 n, d를 출력해 보세요
+	// => 1번째 인자인 문자열을 보면, 나머지 인자의 갯수와 타입을 알수 있습니다.
+	printf("%d%lf", n, d);
 }
